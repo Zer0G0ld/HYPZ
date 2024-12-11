d75e76f80c73976a0f4dfd645340615a008c9036
Zer0G0ld <matheus321trabalho@gmail.com>
2024-12-10 23:40:49 -0300

    atualizações

--- install.sh
+++ install.sh
@@ -1,46 +1,116 @@
 #!/bin/bash
 
-echo "Detectando o shell..."
+# Função para exibir uma mensagem de boas-vindas com estilo
+welcome_message() {
+    clear
+    figlet "HYPZ - Setup" | lolcat
+    echo -e "\e[32mBem-vindo ao HYPZ Setup! Vamos configurar seu shell de uma forma divertida e estilosa!\e[0m"
+    echo ""
+}
 
-# Detectar o shell atual
-SHELL=$(basename $SHELL)
+# Função para instalar dependências
+install_dependencies() {
+    echo -e "\e[34mInstalando dependências necessárias...\e[0m"
+    sudo apt update -y
+    sudo apt install -y figlet zsh cowsay lolcat curl git
+}
 
-# Verifica o shell e configura os arquivos correspondentes
-if [ "$SHELL" = "bash" ]; then
-    # Copia o template de bashrc para o diretório do usuário
-    cp config/bashrc.template ~/.bashrc
-    echo ".bashrc configurado com sucesso!"
-elif [ "$SHELL" = "zsh" ]; then
-    # Copia o template de zshrc para o diretório do usuário
-    cp config/zshrc.tamplate ~/.zshrc
-    echo ".zshrc configurado com sucesso!"
-else
-    echo "Shell não suportado. Apenas Bash e ZSH são suportados."
-    exit 1
-fi
-
-# Instalar dependências
-echo "Instalando dependências necessárias..."
-sudo apt-get update -y
-sudo apt-get install -y zsh figlet
-
-# Verifica se o ZSH foi instalado corretamente
-if ! command -v zsh &> /dev/null; then
-    echo "Erro: O ZSH não foi instalado corretamente."
-    exit 1
-else
-    echo "ZSH instalado com sucesso!"
-fi
-
-# Se o shell for ZSH, pode ser interessante sugerir a instalação do Oh My Zsh
-if [ "$SHELL" = "zsh" ]; then
+# Função para instalar o Oh My Zsh
+install_oh_my_zsh() {
     if [ ! -d "$HOME/.oh-my-zsh" ]; then
-        echo "Oh My Zsh não encontrado. Instalando..."
+        echo -e "\e[34mInstalando Oh My Zsh...\e[0m"
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-        echo "Oh My Zsh instalado com sucesso!"
     else
-        echo "Oh My Zsh já está instalado."
+        echo -e "\e[32mOh My Zsh já está instalado!\e[0m"
     fi
-fi
+}
 
-echo "Instalação concluída!"
+# Função para configurar o Bash
+configure_bash() {
+    echo -e "\e[33mConfigurando o .bashrc...\e[0m"
+    if [ -f "$HOME/.bashrc" ]; then
+        echo -e "\e[31m.bashrc já existe. Fazendo backup...\e[0m"
+        mv "$HOME/.bashrc" "$HOME/.bashrc.backup"
+    fi
+    cp ./config/bashrc.template "$HOME/.bashrc"
+    echo -e "\e[32m.bashrc configurado com sucesso!\e[0m"
+}
+
+# Função para configurar o ZSH
+configure_zsh() {
+    echo -e "\e[33mConfigurando o .zshrc...\e[0m"
+    if [ -f "$HOME/.zshrc" ]; then
+        echo -e "\e[31m.zshrc já existe. Fazendo backup...\e[0m"
+        mv "$HOME/.zshrc" "$HOME/.zshrc.backup"
+    fi
+    cp ./config/zshrc.template "$HOME/.zshrc"
+    echo -e "\e[32m.zshrc configurado com sucesso!\e[0m"
+
+    # Instalar temas e plugins para o ZSH
+    echo -e "\e[34mInstalando temas e plugins do ZSH...\e[0m"
+    mkdir -p "$HOME/.oh-my-zsh/themes"
+    cp ./themes/* "$HOME/.oh-my-zsh/themes/"
+
+    mkdir -p "$HOME/.oh-my-zsh/plugins"
+    cp ./plugins/* "$HOME/.oh-my-zsh/plugins/"
+
+    # Instalar o Oh My Zsh se ainda não estiver instalado
+    install_oh_my_zsh
+}
+
+# Função para verificar se a instalação foi bem-sucedida
+verify_installation() {
+    if [ $? -eq 0 ]; then
+        echo -e "\e[32mInstalação concluída com sucesso!\e[0m"
+    else
+        echo -e "\e[31mOcorreu um erro durante a instalação. Por favor, verifique os logs.\e[0m"
+        exit 1
+    fi
+}
+
+# Menu de escolha de shell
+choose_shell() {
+    while true; do
+        echo -e "\e[32mEscolha o shell que deseja usar:\e[0m"
+        echo "1) Bash"
+        echo "2) ZSH"
+        echo -e "\e[34mDigite o número correspondente (1 ou 2):\e[0m"
+        read -p "Sua escolha: " shell_choice
+
+        if [[ "$shell_choice" == "1" || "$shell_choice" == "2" ]]; then
+            break
+        else
+            echo -e "\e[31mOpção inválida! Por favor, escolha 1 ou 2.\e[0m"
+        fi
+    done
+}
+
+# Função para aplicar a escolha do shell
+apply_choice() {
+    if [ "$shell_choice" -eq 1 ]; then
+        echo -e "\e[33mVocê escolheu o Bash!\e[0m"
+        install_dependencies
+        configure_bash
+        verify_installation
+    elif [ "$shell_choice" -eq 2 ]; then
+        echo -e "\e[33mVocê escolheu o ZSH!\e[0m"
+        install_dependencies
+        configure_zsh
+        verify_installation
+        # Mudar para ZSH se escolhido
+        chsh -s $(which zsh)
+        echo -e "\e[32mZSH será o seu shell padrão! Reinicie o terminal para aplicar.\e[0m"
+    fi
+}
+
+# Finalização
+finish_up() {
+    echo -e "\e[32mInstalação concluída com sucesso! Agora é só se divertir com o seu novo shell!\e[0m"
+    echo -e "\e[33mReinicie o terminal para que todas as mudanças tenham efeito.\e[0m"
+}
+
+# Iniciar o processo
+welcome_message
+choose_shell
+apply_choice
+finish_up
--- scripts/customize.sh
+++ scripts/customize.sh
@@ -1,34 +1,60 @@
 #!/bin/bash
 
+# Função para exibir uma mensagem de boas-vindas
+welcome_message() {
+    echo -e "\e[32mBem-vindo ao HYPZ Customizer!\e[0m"
+}
+
 # Função para atualizar o prompt
 update_prompt() {
     local shell_config=$1
     local prompt_style=$2
 
-    echo "Atualizando prompt no arquivo $shell_config..."
+    echo -e "\e[34mAtualizando prompt no arquivo $shell_config...\e[0m"
+    
+    if [ ! -f "$shell_config" ]; then
+        echo -e "\e[31mArquivo de configuração não encontrado: $shell_config\e[0m"
+        return 1
+    fi
+
     if [ "$prompt_style" = "default" ]; then
         echo 'export PS1="%F{green}%n@%m %F{cyan}%~%f %# "' >> "$shell_config"
     elif [ "$prompt_style" = "minimalist" ]; then
         echo 'export PS1="%F{yellow}%n@%m%f %# "' >> "$shell_config"
     else
-        echo "Estilo de prompt não reconhecido!"
+        echo -e "\e[31mEstilo de prompt não reconhecido! Escolha entre 'default' ou 'minimalist'.\e[0m"
+        return 1
     fi
+    echo -e "\e[32mPrompt atualizado com sucesso!\e[0m"
 }
 
 # Função para adicionar aliases personalizados
 add_custom_aliases() {
     local shell_config=$1
 
-    echo "Adicionando aliases personalizados em $shell_config..."
+    echo -e "\e[34mAdicionando aliases personalizados em $shell_config...\e[0m"
+
+    if [ ! -f "$shell_config" ]; then
+        echo -e "\e[31mArquivo de configuração não encontrado: $shell_config\e[0m"
+        return 1
+    fi
+
     echo "alias gs='git status'" >> "$shell_config"
     echo "alias ga='git add'" >> "$shell_config"
     echo "alias gl='git log'" >> "$shell_config"
+
+    echo -e "\e[32mAliases personalizados adicionados com sucesso!\e[0m"
 }
 
 # Função para escolher o tema
 choose_theme() {
     local theme=$1
-    echo "Escolhendo tema $theme..."
+    echo -e "\e[34mEscolhendo tema: $theme...\e[0m"
+
+    if [ ! -d "$HYPZ/themes" ]; then
+        echo -e "\e[31mDiretório de temas não encontrado! Verifique sua instalação do HYPZ.\e[0m"
+        return 1
+    fi
 
     if [ "$theme" = "default" ]; then
         cp "$HYPZ/themes/default.zsh-theme" "$HOME/.zsh-theme"
@@ -37,44 +63,66 @@ choose_theme() {
     elif [ "$theme" = "vibrant" ]; then
         cp "$HYPZ/themes/vibrant.zsh-theme" "$HOME/.zsh-theme"
     else
-        echo "Tema não encontrado!"
+        echo -e "\e[31mTema não encontrado! Escolha entre 'default', 'minimalist' ou 'vibrant'.\e[0m"
+        return 1
     fi
+    echo -e "\e[32mTema $theme aplicado com sucesso!\e[0m"
 }
 
-# Perguntar ao usuário qual personalização ele deseja
-echo "Bem-vindo ao HYPZ Customizer!"
-echo "Escolha uma opção de personalização:"
-echo "1. Atualizar o prompt"
-echo "2. Adicionar aliases personalizados"
-echo "3. Escolher tema"
-echo "4. Sair"
+# Função para recarregar configurações do shell
+reload_shell() {
+    echo -e "\e[34mRecarregando configurações do shell...\e[0m"
+    source "$HOME/.zshrc"
+    echo -e "\e[32mConfigurações recarregadas com sucesso!\e[0m"
+}
 
-read -p "Digite o número da opção: " option
+# Função para exibir o menu e processar a escolha do usuário
+choose_option() {
+    echo -e "\nEscolha uma opção de personalização:"
+    echo "1. Atualizar o prompt"
+    echo "2. Adicionar aliases personalizados"
+    echo "3. Escolher tema"
+    echo "4. Sair"
+    read -p "Digite o número da opção: " option
+}
 
-case $option in
-    1)
-        echo "Escolha o estilo de prompt (default/minimalist):"
-        read prompt_style
-        update_prompt "$HOME/.zshrc" "$prompt_style"
-        ;;
-    2)
-        add_custom_aliases "$HOME/.zshrc"
-        ;;
-    3)
-        echo "Escolha o tema (default/minimalist/vibrant):"
-        read theme_choice
-        choose_theme "$theme_choice"
-        ;;
-    4)
-        echo "Saindo do personalizador."
-        exit 0
-        ;;
-    *)
-        echo "Opção inválida!"
-        ;;
-esac
+# Função principal para gerenciar o menu
+customize() {
+    while true; do
+        choose_option
+        
+        case $option in
+            1)
+                echo "Escolha o estilo de prompt (default/minimalist):"
+                read prompt_style
+                update_prompt "$HOME/.zshrc" "$prompt_style"
+                ;;
+            2)
+                add_custom_aliases "$HOME/.zshrc"
+                ;;
+            3)
+                echo "Escolha o tema (default/minimalist/vibrant):"
+                read theme_choice
+                choose_theme "$theme_choice"
+                ;;
+            4)
+                echo -e "\e[32mSaindo do personalizador. Até logo!\e[0m"
+                break
+                ;;
+            *)
+                echo -e "\e[31mOpção inválida! Por favor, escolha 1, 2, 3 ou 4.\e[0m"
+                ;;
+        esac
 
-# Recarregar configurações do shell
-echo "Recarregando configurações..."
-source "$HOME/.zshrc"
-echo "Personalização concluída!"
+        # Se o arquivo foi modificado, recarregar o shell
+        echo -e "\e[34mDeseja recarregar as configurações do shell agora? (y/n)\e[0m"
+        read reload_choice
+        if [[ "$reload_choice" =~ ^[Yy]$ ]]; then
+            reload_shell
+        fi
+    done
+}
+
+# Iniciar o processo
+welcome_message
+customize
